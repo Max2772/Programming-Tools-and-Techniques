143513830a34476181eba6b88bbae5bafc71ef7e
Bibikau <bib.maxim@gmail.com>
2026-03-14 12:23:32 +0300

    refactor(LB3): add simple calculator page with shell

---
+++ Part2/LB3/LB3/CalculatorPage.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LB3;
+
+public partial class CalculatorPage : ContentPage
+{
+    String input="";
+    public CalculatorPage()
+    {
+        InitializeComponent();
+    }
+
+    private void PressButton(object sender, EventArgs e)
+    {
+        if (sender is Button button)
+        {
+
+            if(button.Text=="exp(x)")
+            {
+                double res = 0;
+                bool no_exc = true;
+                if(double.TryParse(input,out res))
+                {
+                    try
+                    {
+                        res=Math.Exp(res);
+                    }
+                    catch(Exception ex)
+                    {
+                        no_exc = false;
+                    }
+
+                    if (no_exc)
+                    {
+                        input="";
+                        DisplayLabel.Text = res.ToString();
+                        return;
+                    }
+                      
+                }
+
+                DisplayLabel.Text = "Error";
+                input = "";
+                return;
+            }
+
+            if(button.Text== "C")
+            {
+                if(input.Length>0)
+                    input = input.Remove(input.Length - 1);
+            }
+            else
+                input += button.Text;
+
+            DisplayLabel.Text = input;
+            
+        }
+    }
+}
