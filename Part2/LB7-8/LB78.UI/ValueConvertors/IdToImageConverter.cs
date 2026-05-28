6ed7bdd4602d3b20b67e51d42aa3d2a70d30f8f9
Max2772 <bib.maxim@gmail.com>
2026-05-28 14:27:09 +0300

    fix(LB7-8): newlines fix in files caused by IDEA

--- Part2/LB7-8/LB78.UI/ValueConvertors/IdToImageConverter.cs
+++ Part2/LB7-8/LB78.UI/ValueConvertors/IdToImageConverter.cs
@@ -1,24 +1,1 @@
-using System.Globalization;
-using LB78.UI.Services;
-
-namespace LB78.UI.ValueConvertors;
-
-class IdToImageConverter : IValueConverter
-{
-    public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
-    {
-        if (value is int id && id > 0)
-        {
-            var path = ImageStorageService.GetImagePath(id);
-            if (File.Exists(path))
-                return path;
-        }
-
-        return "dotnet_bot.png";
-    }
-
-    public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
-    {
-        throw new NotImplementedException();
-    }
-}
+using System.Globalization;using LB78.UI.Services;namespace LB78.UI.ValueConvertors;class IdToImageConverter : IValueConverter{    public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)    {        if (value is int id && id > 0)        {            var path = ImageStorageService.GetImagePath(id);            if (File.Exists(path))                return path;        }        return "dotnet_bot.png";    }    public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)    {        throw new NotImplementedException();    }}
--- Part2/LB7-8/LB78.UI/ViewModels/SushiDetailsViewModel.cs
+++ Part2/LB7-8/LB78.UI/ViewModels/SushiDetailsViewModel.cs
@@ -1,53 +1,1 @@
-using CommunityToolkit.Mvvm.ComponentModel;
-using CommunityToolkit.Mvvm.Input;
-using LB78.Application.SushiUseCases.Commands;
-using LB78.Application.SushiUseCases.Queries;
-using LB78.UI.Pages;
-
-namespace LB78.UI.ViewModels;
-
-[QueryProperty("Sushi", "Sushi")]
-public partial class SushiDetailsViewModel(IMediator mediator) : ObservableObject
-{
-    [ObservableProperty] private Sushi sushi = null!;
-
-    [ObservableProperty] string name = string.Empty;
-    [ObservableProperty] int readyCount;
-    [ObservableProperty] decimal weight;
-    [ObservableProperty] string description = string.Empty;
-
-    [RelayCommand]
-    async Task GetByIdSushi()
-    {
-        Sushi = (await mediator.Send(new GetByIdSushiRequest(Sushi.Id)))!;
-        Name = Sushi.Name;
-        ReadyCount = Sushi.ReadyCount;
-        Weight = Sushi.Weight;
-        Description = Sushi.Description;
-    }
-
-    [RelayCommand]
-    async Task EditSushi()
-    {
-        await Shell.Current.GoToAsync(nameof(AddOrEditSushiWindow), new Dictionary<string, object>()
-        {
-            { "Request", new EditSushiRequest() { Sushi = Sushi } }
-        });
-    }
-
-    [RelayCommand]
-    async Task DeleteSushi()
-    {
-        bool confirm = await Shell.Current.DisplayAlert(
-            "Удаление",
-            $"Удалить суши «{Sushi.Name}»?",
-            "Да",
-            "Нет");
-
-        if (!confirm)
-            return;
-
-        await mediator.Send(new DeleteSushiRequest(Sushi));
-        await Shell.Current.GoToAsync("..");
-    }
-}
+using CommunityToolkit.Mvvm.ComponentModel;using CommunityToolkit.Mvvm.Input;using LB78.Application.SushiUseCases.Commands;using LB78.Application.SushiUseCases.Queries;using LB78.UI.Pages;namespace LB78.UI.ViewModels;[QueryProperty("Sushi", "Sushi")]public partial class SushiDetailsViewModel(IMediator mediator) : ObservableObject{    [ObservableProperty] private Sushi sushi = null!;    [ObservableProperty] string name = string.Empty;    [ObservableProperty] int readyCount;    [ObservableProperty] decimal weight;    [ObservableProperty] string description = string.Empty;    [RelayCommand]    async Task GetByIdSushi()    {        Sushi = (await mediator.Send(new GetByIdSushiRequest(Sushi.Id)))!;        Name = Sushi.Name;        ReadyCount = Sushi.ReadyCount;        Weight = Sushi.Weight;        Description = Sushi.Description;    }    [RelayCommand]    async Task EditSushi()    {        await Shell.Current.GoToAsync(nameof(AddOrEditSushiWindow), new Dictionary<string, object>()        {            { "Request", new EditSushiRequest() { Sushi = Sushi } }        });    }    [RelayCommand]    async Task DeleteSushi()    {        bool confirm = await Shell.Current.DisplayAlert(            "Удаление",            $"Удалить суши «{Sushi.Name}»?",            "Да",            "Нет");        if (!confirm)            return;        await mediator.Send(new DeleteSushiRequest(Sushi));        await Shell.Current.GoToAsync("..");    }}
